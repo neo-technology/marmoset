f9d696c6855cc1815390db72d4cb20f25dc454e4
Jacob Davis-Hansson <jakewins@gmail.com>
2018-12-10 13:00:52 -0600

    Change default json logging fields
    From "msg" to "message" and "lvl" to "level", to fit
    with stackdriver defaults.

--- chaoskube/logging_test.go
+++ chaoskube/logging_test.go
@@ -1,11 +1,12 @@
 package chaoskube_test
 
 import (
+	"bytes"
 	"github.com/neo-technology/marmoset/chaoskube"
-	"testing"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/suite"
 	"os"
+	"testing"
 )
 
 type Suite struct {
@@ -24,5 +25,22 @@ func (suite *Suite) TestSetsDefaultFields() {
 	suite.Equal(log.Fields{"banana": "boo"}, entry.Data)
 	suite.Equal(log.InfoLevel, entry.Logger.Level)
 	suite.Equal(os.Stdout, entry.Logger.Out)
-	suite.Equal(&log.JSONFormatter{}, entry.Logger.Formatter)
+	suite.Equal(&log.JSONFormatter{
+		FieldMap: log.FieldMap{
+			log.FieldKeyMsg: "message",
+			log.FieldKeyLevel: "severity",
+		},
+	}, entry.Logger.Formatter)
+}
+
+func (suite *Suite) TestDefaultFieldNames() {
+	logger := chaoskube.SetupLogging(false, "json", "banana=boo")
+
+	buf := new(bytes.Buffer)
+	logger.(*log.Entry).Logger.Out = buf
+
+	logger.Info("hello, world!")
+
+	suite.Contains(buf.String(), "\"message\":\"hello, world!\"")
+	suite.Contains(buf.String(), "\"severity\":\"info\"")
 }
